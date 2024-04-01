0fd7713881c311cf97dcd9ea9dece4da482be3f8
Elitsuv <work.for.jeez999@gmail.com>
2024-04-01 22:46:57 +0530

    New change #2

--- src/script.js
+++ src/script.js
@@ -1,9 +1,19 @@
 window.alert("This website is under Pre development");
 
-const hamburgerMenu = document.querySelector('.hamburger-menu');
-const navHead = document.querySelector('.nav-head');
+document.addEventListener('DOMContentLoaded', function () {
+  const hamburgerMenu = document.querySelector('.hamburger-menu');
+  const navHead = document.querySelector('.nav-head');
 
-hamburgerMenu.addEventListener('click', () => {
-  hamburgerMenu.classList.toggle('active');
-  navHead.classList.toggle('active');
+  hamburgerMenu.addEventListener('click', () => {
+      hamburgerMenu.classList.toggle('active');
+      navHead.classList.toggle('active');
+  });
+
+  // Close hamburger menu when clicking on a navigation link
+  document.querySelectorAll('.nav-head a').forEach(item => {
+      item.addEventListener('click', () => {
+          hamburgerMenu.classList.remove('active');
+          navHead.classList.remove('active');
+      });
+  });
 });
