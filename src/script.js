5d73fef2f5d1541ec957a663a42522baf6a7d4be
Elitsuv <work.for.jeez999@gmail.com>
2024-04-01 19:01:31 +0530

    Major Changes wuth new design

--- src/script.js
+++ src/script.js
@@ -1,19 +1,9 @@
 window.alert("This website is under Pre development");
 
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const navHead = document.querySelector('.nav-head');
 
-function search() {
-    let textToSearch = document.getElementById("search-box").value;
-    let mainBody = document.querySelector(".main-b1");
-
-    if (!mainBody) {
-        alert("Element with class 'main-b1' not found.");
-        return;
-    }
-
-    textToSearch = textToSearch.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
-
-    let pattern = new RegExp(textToSearch, "gi");
-
-    mainBody.innerHTML = mainBody.innerHTML.replace(pattern, match => `<mark>${match}</mark>`);
-}
-
+hamburgerMenu.addEventListener('click', () => {
+  hamburgerMenu.classList.toggle('active');
+  navHead.classList.toggle('active');
+});
